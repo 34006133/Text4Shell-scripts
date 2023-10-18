68f44737749ad677de3dc25181e33bcc105bff4f
34006133 <143711860+34006133@users.noreply.github.com>
2023-10-18 19:01:21 +0800

    Update server.sh

--- server.sh
+++ server.sh
@@ -49,3 +49,32 @@ ACTIVE_RESPONSE_CONF="<ossec_config>
 echo $ACTIVE_RESPONSE_CONF >> /var/ossec/etc/ossec.conf
 
 systemctl restart wazuh-manager
+
+# Command Detection
+CDB_LIST="ncat:red
+\nbash:red
+\ncurl:red
+\nnc:red
+"
+echo $CDB_LIST >> /var/ossec/etc/lists/sus-commands
+
+RULESET="<ossec_config>
+  <ruleset>
+    <list>etc/lists/sus-commands</list>
+  </ruleset>
+</ossec_config>
+"
+echo $RULESET >> /var/ossec/etc/ossec.conf
+
+COMMAND_RULE="<group name=\"audit\">
+  <rule id="100210" level="12">
+      <if_sid>80792</if_sid>
+  <list field=\"audit.command\" lookup=\"match_key_value\" check_value=\"red\">etc/lists/sus-commands</list>
+    <description>Audit: Highly Suspicious Command executed: \$(audit.exe)</description>
+      <group>audit_command,</group>
+  </rule>
+</group>
+"
+echo $COMMAND_RULE >> /var/ossec/etc/rules/local_rules.xml
+
+systemctl restart wazuh-manager
